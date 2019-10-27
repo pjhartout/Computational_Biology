cd0f86d6409db18489fa4104cd921d7a2eebf56d
Philip Hartout <philip.hartout@protonmail.com>
2019-10-27 20:59:36 +0100

    :pencil: change directory name for test directory

--- assignments/assignment_2/run_tests.R
+++ assignments/assignment_2/run_tests.R
@@ -1,5 +1,5 @@
-tests_dir <- "~/Dropbox/ETHZ/Semester_1/Computational_Biology/Assignments/Assignment_2/student_test_suite/"
-if(tests_dir == "~/Dropbox/ETHZ/Semester_1/Computational_Biology/Assignments/Assignment_2/run_tests.R") stop("tests_dir needs to be set to a proper path")
+tests_dir <- "~/Dropbox/ETHZ/Semester_1/Computational_Biology/assignments/assignment_2/student_test_suite/"
+if(tests_dir == "~/Dropbox/ETHZ/Semester_1/Computational_Biology/assignments/assignment_2/run_tests.R") stop("tests_dir needs to be set to a proper path")
 
 library("RUnit")
 
