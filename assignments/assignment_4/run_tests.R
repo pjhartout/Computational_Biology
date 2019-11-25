35afbccc87c6cf2c85c98ba2ea887912ca906508
Philip Hartout <philip.hartout@protonmail.com>
2019-11-25 11:21:20 +0100

    :gear: change line endings and testing directory

--- assignments/assignment_4/run_tests.R
+++ assignments/assignment_4/run_tests.R
@@ -1,4 +1,4 @@
-tests_dir <- "C:/Users/phili/Dropbox/ETHZ/Semester_1/Computational_Biology/assignments/assignment_4/student_test_suite"
+tests_dir <- "/home/pjhartout/Dropbox/ETHZ/Semester_1/Computational_Biology/assignments/assignment_4/student_test_suite/"
 library("RUnit")
 
 testsuite <- defineTestSuite("HW", tests_dir)
